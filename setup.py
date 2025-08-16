5846fd99e623fcf4d7838870ab86122995ea714a
ADIL AIJAZ <adilaijaz10@gmail.com>
2025-08-16 22:37:47 +0500

    Initialize project structure and dependencies
    Added initial project files including app.py, requirements.txt, setup.py, and source directories. Updated .gitignore to exclude template.py. This sets up the basic structure for a generative AI project with relevant dependencies and package configuration.

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="Generative AI Project",
+    version="0.0.0",
+    author="Adil Aijaz",
+    author_email="adilaijaz10@gmail.com",
+    packages=find_packages(), #it will consider __init__.py as local package
+    install_requires=[],
+)
