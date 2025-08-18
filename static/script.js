3378ae5ccba8bbcccf1ab0b13ad98dc1026c03d7
ADIL AIJAZ <adilaijaz10@gmail.com>
2025-08-19 00:19:30 +0500

    Add chat frontend script and improve responsiveness
    Introduced static/script.js to handle chat interactions, message sending, and bot responses. Updated style.css to increase chat box size and add responsive styles for various screen sizes. Modified index.html to include the new script and improve script loading order.

---
+++ static/script.js
@@ -0,0 +1,86 @@
+var $messages = $('.messages-content'),
+    d, h, m,
+    i = 0;
+
+$(window).load(function () {
+    $messages.mCustomScrollbar();
+    setTimeout(function () {
+        // Welcome message instead of fake message
+        showBotMessage("Hello! I'm a medical assistant. How can I help you today?");
+    }, 100);
+});
+
+function updateScrollbar() {
+    $messages.mCustomScrollbar("update").mCustomScrollbar('scrollTo', 'bottom', {
+        scrollInertia: 10,
+        timeout: 0
+    });
+}
+
+function setDate() {
+    d = new Date()
+    if (m != d.getMinutes()) {
+        m = d.getMinutes();
+        $('<div class="timestamp">' + d.getHours() + ':' + m + '</div>').appendTo($('.message:last'));
+    }
+}
+
+function insertMessage() {
+    msg = $('.message-input').val();
+    if ($.trim(msg) == '') {
+        return false;
+    }
+    $('<div class="message message-personal">' + msg + '</div>').appendTo($('.mCSB_container')).addClass('new');
+    setDate();
+    $('.message-input').val(null);
+    updateScrollbar();
+    
+    // Show typing indicator
+    showLoading();
+    
+    // Send message to backend and get response
+    $.ajax({
+        url: '/get',
+        type: 'POST',
+        data: {
+            msg: msg
+        },
+        success: function(response) {
+            // Remove loading message
+            $('.message.loading').remove();
+            
+            // Display the response from the LLM
+            showBotMessage(response);
+        },
+        error: function(error) {
+            // Remove loading message
+            $('.message.loading').remove();
+            
+            // Show error message
+            showBotMessage("Sorry, I encountered an error. Please try again.");
+            console.error("Error:", error);
+        }
+    });
+}
+
+function showLoading() {
+    $('<div class="message loading new"><figure class="avatar"><img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/156381/profile/profile-80.jpg" /></figure><span></span></div>').appendTo($('.mCSB_container'));
+    updateScrollbar();
+}
+
+function showBotMessage(message) {
+    $('<div class="message new"><figure class="avatar"><img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/156381/profile/profile-80.jpg" /></figure>' + message + '</div>').appendTo($('.mCSB_container')).addClass('new');
+    setDate();
+    updateScrollbar();
+}
+
+$('.message-submit').click(function () {
+    insertMessage();
+});
+
+$(window).on('keydown', function (e) {
+    if (e.which == 13) {
+        insertMessage();
+        return false;
+    }
+})
